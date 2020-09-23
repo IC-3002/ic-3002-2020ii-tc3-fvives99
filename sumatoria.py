e5771a86de5700422f91743728c8a49da18aaa97
Fabian Vives <f.vives99@gmail.com>
2020-09-23 17:09:54 -0600

    tc3

--- sumatoria.py
+++ sumatoria.py
@@ -1,6 +1,17 @@
 def sumatoria_cubica(n):
-    raise NotImplementedError()
-
+    r=0
+    for i in range(1, n+1):
+        for j in range(1, i+1):
+            for k in range(j,i+j+1):
+                r+=1
+    return r
+    #raise NotImplementedError()
+    #sumatoria_cubica(5)
+    
 def sumatoria_constante(n):
-    raise NotImplementedError()
-
+    i = (n+1)*n
+    j = ((n+2)/3)
+    return int(i*j)
+    #raise NotImplementedError()
+    #print (sumatoria_constante(5))
+    
